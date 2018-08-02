5821ca97c72442fa42e0bb10f7c2a929623cb86f
Timothy Lim <timothyws.lim@gmail.com>
2018-08-02 21:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(name){
   return 'Happy holidays, ${name}!'
 }
 function happyHolidayTo(holiday,name){
-  return 'Happy ${holiday},${name}!'
+  return 'Happy ${holiday}, ${name}!'
 }
 function holidayCountdown(holiday,days){
   return 'Its ${days} days until ${holiday}!'
